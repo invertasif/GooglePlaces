1807754f4655bb9a4cbbcad7b1dd4947e1638814
Shridar Ananda <Shridar.Ananda@altimetrik.com>
2017-04-16 22:21:38 +0530

    Project deleted

--- WebonoiseAssignment/Pods/Target Support Files/KSReachability/KSReachability-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KSReachability : NSObject
-@end
-@implementation PodsDummy_KSReachability
-@end
--- WebonoiseAssignment/Pods/Target Support Files/KSReachability/KSReachability-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "KSReachability.h"
-
-FOUNDATION_EXPORT double KSReachabilityVersionNumber;
-FOUNDATION_EXPORT const unsigned char KSReachabilityVersionString[];
-
